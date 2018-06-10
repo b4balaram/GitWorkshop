d9c402b0ef7ee1a4802a00f1d8f81b08304ba22f
Sonu <Sonu@Java_Developer>
2018-06-10 18:32:00 +0530

    change by tl

--- GitProj1/src/com/nt/git/Test.java
+++ GitProj1/src/com/nt/git/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Welcome to  Git");
 		System.out.println("hiii");
-//	do work fast	
+		System.out.println("Good Evening");
 	
 	}
 
